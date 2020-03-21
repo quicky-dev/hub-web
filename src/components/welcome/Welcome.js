a89cdf1823862e71a0d768e8cb9d5dc456bec24d
Timothy Kaing <kaing.timothy@gmail.com>
2020-03-20 18:25:48 -0700

    set up basic welcome section

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Welcome.css";
 
 export class Welcome extends Component {
-    render() {
-        return(
-            <div className="welcome">
-                <p>hello</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="welcome">
+        <section className="container">
+          <div className="left">
+            <h1>The Hub.</h1>
+          </div>
+          <div className="right">
+            {/* Signup/Login Form Component Here */}
+            <h3>Form</h3>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Welcome;
