13603cde8d5155045224da92429fcfcb2fd005c7
Timothy Kaing <kaing.timothy@gmail.com>
2020-03-23 23:00:28 -0700

    create script component to render on profile page

---
+++ src/components/profile-script/ProfileScript.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../profile-script/ProfileScript.css";
+
+class ProfileScript extends Component {
+    render() {
+        return (
+            <div className="script">
+                <h4 href="#">{this.props.name}</h4>
+                {/* tags can go here and be css styled */}
+                {/* or they can be a component */}
+            </div>
+        )
+    }
+}
+
+export default ProfileScript;
